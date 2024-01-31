6ce9212b8fed8b76d70d6f48745d3a99334c08ad
Bittu <sathwik@Sathwiks-MacBook-Pro.local>
2024-01-31 20:25:11 +0530

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex flex-col items-center bg-[#1c1c24">
+      
+      <div className="font-epilogue font-normal text-[16px] text-[#808191] bg-[#28282e] px-3 py-2 w-full rouned-b-[10px] text-center">
+        <div>
+          <p>Developed by Rajeshwar Manthena </p>
+        </div>
+        
+        <a href="mailto:rajeshwarmanthena11@gmail.com.com">Mail</a> | 
+        <a href="https://www.linkedin.com/in/rajeshwar-manthena-362b28268/"> LinkedIn</a> | 
+        <a href="https://github.com/rajeshwarmanthena">  Github</a>
+
+        </div>
+    </div>
+    </footer>
+  );
+};
+
+export default Footer;
